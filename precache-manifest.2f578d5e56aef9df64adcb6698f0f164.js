c8f5f8713ed0a340546f1cd6851debad026bb9ec
Dhruv Verma <dhruv2scs@gmail.com>
2020-04-16 17:35:44 +0530

    Updates

---
+++ precache-manifest.2f578d5e56aef9df64adcb6698f0f164.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076176a9eb32862ae13871bf321792bf",
+    "url": "/Reddit-flair-classifier/index.html"
+  },
+  {
+    "revision": "15a3c66f95cbf0ef5493",
+    "url": "/Reddit-flair-classifier/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "5123e5c39435183a3d1f",
+    "url": "/Reddit-flair-classifier/static/css/main.977e8f8c.chunk.css"
+  },
+  {
+    "revision": "15a3c66f95cbf0ef5493",
+    "url": "/Reddit-flair-classifier/static/js/2.fc569f8f.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Reddit-flair-classifier/static/js/2.fc569f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5123e5c39435183a3d1f",
+    "url": "/Reddit-flair-classifier/static/js/main.6e97a444.chunk.js"
+  },
+  {
+    "revision": "43e21247306211f245b6",
+    "url": "/Reddit-flair-classifier/static/js/runtime-main.d0987230.js"
+  },
+  {
+    "revision": "7dbc3631ca8b506271e6c3f724e1549f",
+    "url": "/Reddit-flair-classifier/static/media/reddit_logo.7dbc3631.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Reddit-flair-classifier-frontend/precache-manifest.01da105cd3e55063c146d62954fe4606.js"
+  "/Reddit-flair-classifier/precache-manifest.2f578d5e56aef9df64adcb6698f0f164.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Reddit-flair-classifier-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Reddit-flair-classifier/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
